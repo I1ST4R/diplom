3d0debac65efc543fe279f4e28bec2940f2304da
Vanya <reshetilov-vanechka@mail.ru>
2026-02-03 09:59:04 +0300

    feat: create backend and frontend min

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Привет с backend 👋' });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Backend server listening on http://localhost:${PORT}`);
+});
+
